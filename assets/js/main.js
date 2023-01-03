1b8c02a7346de038c8684f47c95338c6fe9b1590
karinlee <karinlee92@gmail.com>
2023-01-03 12:06:37 +0100

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+/* Mehrzeiliger
+Kommentar */
+
+// einzeiliger Kommentar
+
+let helloWorld= "Hello World";
+console.log(helloWorld);
+
+const pi =3.14;
+console.log(pi);
+
+let burjDubai= "828m";
+console.log(burjDubai);
+
+let fullName = "Jan Schmidt";
+console.log(fullName);
+
+const eifelTower= "324m";
+console.log(eifelTower);
+
+const camelCase = "CamelCase";
+console.log(camelCase);
+
+let kursStatus = (true);
+console.log(kursStatus);  
